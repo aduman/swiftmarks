b1b19a5a2101b05d6a8e7f604e9b5b70dfeb2206
Josh O'Rourke <joshorourke@me.com>
2010-12-04 17:31:12 -0500

    Added tests for UserSessionsController.

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should GET :new" do
+    get :new
+    assert_response :success
+    assert_template 'new'
+  end
+
+  test "should create user session" do
+    post :create, :user_session => { :email => "josh@localhost", :password => "secret123" }
+    assert user_session = UserSession.find
+    assert_equal users(:josh), user_session.user
+    assert_redirected_to bookmarks_url
+  end
+
+  test "should destroy user session" do
+    delete :destroy
+    assert_nil UserSession.find
+    assert_redirected_to new_user_session_path
   end
 end
