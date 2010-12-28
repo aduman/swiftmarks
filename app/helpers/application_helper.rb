9e07832386a19a1e9fdd50a06e6ffc5728bc9404
Josh O'Rourke <joshorourke@me.com>
2010-12-28 18:59:17 -0500

    Added bookmarklet_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def bookmarklet_url
+    "javascript:" +
+    "document.location='#{new_bookmark_url(:only_path => false)}'+" + 
+    "'?source=bookmarklet'+" +
+    "'&url='+encodeURIComponent(location.href)+" +
+    "'&title='+encodeURIComponent(document.title)"
+  end
 end
