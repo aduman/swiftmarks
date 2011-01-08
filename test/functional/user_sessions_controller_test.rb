94e0b18430bc0df30dc2a4013238155bf738f593
Josh O'Rourke <joshorourke@me.com>
2011-01-08 01:24:18 -0500

    Made test names BDD friendly

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,20 +1,21 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-  test "should GET :new" do
+  test "new should load a new UserSession and render page" do
     get :new
+    assert assigns(:user_session).new_record?
     assert_response :success
     assert_template 'new'
   end
 
-  test "should create user session" do
+  test "create should sign in user and redirect to bookmarks list page" do
     post :create, :user_session => { :email => "joshorourke@me.com", :password => "secret123" }
     assert user_session = UserSession.find
     assert_equal users(:josh), user_session.user
     assert_redirected_to bookmarks_url
   end
 
-  test "should destroy user session" do
+  test "destroy should sign out user and redirect to sign in page" do
     delete :destroy
     assert_nil UserSession.find
     assert_redirected_to new_user_session_path
