34d7ff497e73c04d239c1dd236645fd91b1b830c
Josh O'Rourke <joshorourke@me.com>
2011-02-08 23:57:03 -0500

    Add bookmark_url_host to bookmarks_helper

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,9 @@
 module BookmarksHelper
+  def bookmark_url_host(bookmark)
+    if bookmark && bookmark.uri
+      "(#{bookmark.uri.host})"
+    else
+      ""
+    end
+  end
 end
--- test/unit/helpers/bookmarks_helper_test.rb
+++ test/unit/helpers/bookmarks_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class BookmarksHelperTest < ActionView::TestCase
+  test "bookmark_url_host should return formatted host when uri is present" do
+    bookmark = Bookmark.new(:url => "http://www.example.com")
+    assert "(www.example.com)", bookmark_url_host(bookmark)
+  end
+
+  test "bookmark_url_host should return an empty string when uri is nil" do
+    bookmark = Bookmark.new
+    assert "", bookmark_url_host(bookmark)
+  end
 end
