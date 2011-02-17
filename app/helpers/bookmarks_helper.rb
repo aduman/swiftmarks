94d63acc6fc7c886c8d75e258a2a59b26820d857
Josh O'Rourke <joshorourke@me.com>
2011-02-16 22:26:36 -0500

    Use link_to_remote_script instead of link_to

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -22,7 +22,7 @@ module BookmarksHelper
   def link_to_next_page
     if @bookmarks && @bookmarks.next_page
       next_page = params.merge(:page => @bookmarks.next_page)
-      link_to "More", next_page, :remote => true, "data-type" => "script", :id => "next-page"
+      link_to_remote_script "More", next_page, :id => "next-page"
     end
   end
 end
