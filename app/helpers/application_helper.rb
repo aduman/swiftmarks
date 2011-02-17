9dc7ea80a2304fbc28d0a979cc230e50a98d7785
Josh O'Rourke <joshorourke@me.com>
2011-02-16 22:26:01 -0500

    Add link_to_remote_script and options_for_remote_script helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
     "'&url='+encodeURIComponent(location.href)+" +
     "'&title='+encodeURIComponent(document.title)"
   end
+
+  def link_to_remote_script(*args, &block)
+    if block_given?
+      name         = capture(&block) 
+      options      = args[0] || {}
+      html_options = args[1] || {}
+    else
+      name         = args[0]
+      options      = args[1] || {}
+      html_options = args[2] || {}
+    end
+
+    link_to(name, options, html_options.merge(options_for_remote_script))
+  end
+
+  def options_for_remote_script
+    { :remote => true, "data-type" => "script" }
+  end
 end
